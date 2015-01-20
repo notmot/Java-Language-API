f97ce86b3aa40ea7d22e5ddcbaf7248485fe8a3a
null <yoloswag@stuff.in>
2015-01-19 21:40:38 -0600

    Added part of the reword function.

--- Words/src/words/Main.java
+++ Words/src/words/Main.java
@@ -1,35 +1,12 @@
 package words;
 
 import java.io.IOException;
-//	String[] set = {"fuck your","noun","fuck your","noun","fuck your","noun","and fuck your","adj","ass","noun","bitch, this is America"};
-//String[] set = {"prep","the","adj","noun","of the","adj","noun", ""};
-	//String[] set = {"prep","the","adj","noun","of the","adj","noun","that","verb","the","adj","noun"};
-	//String[] set = {"prep","the","noun"};
 public class Main {
 	//public static String[] read=null;
 	//TODO: make plural and singular nouns seperat.
-	public static String buf = "";
-	static String fulltext =   "x states that the noun is adverb absolute";
-	public static String[] wrds = fulltext.split(" ");
-	public static String[] read = fulltext.split(" ");//that is fucked up if you fuck up the lenght
 	public static void main(String[] args) throws InterruptedException, IOException {
-		for(int i=0;i<wrds.length;i++){
-			buf = wrds[i];
-			//System.out.print("\""+words.Types.gettype(buf)+"\",");
-			read[i]=words.Types.gettype(buf);
-		}
-	String[] set = {"x states that","the","noun","is","adverb", "low"};
-	//String[] set = {"noun","verb",";","because","noun","are","adj"};
-	while(true){
-	
-	for(int i=0; i<set.length; i++){
-	      String ret ="";
-	      String now = words.Types.randomthing(set[i])+" ";
-	      System.out.print(now);
-	}
-	System.out.println();
-	Thread.sleep(500);
-}
+		System.out.println(words.Read_Write.structure("walk the dog"));
+
 }
 }
 			
---
+++ Words/src/words/Read_Write.java
@@ -0,0 +1,21 @@
+package words;
+
+import java.io.IOException;
+
+public class Read_Write {
+public static String reword(String text){
+	return null;
+}
+public static String RemoveEndPunctuation(String word){
+	return word.replace(".", "").replace("?", "").replace(";", "").replace("!", "");
+	
+}
+public static String structure(String words){
+	String ret = "";
+	String[] word = RemoveEndPunctuation(words).split(" ");
+	for(int i=0;i<word.length;i++){
+		ret=ret+" "+Types.gettype(word[i]);
+	}	
+	return ret;	
+}
+}
